d84b7971966dc204d7e4e5214979e7765e2c559b
Flavio <FlavioAmurrioCS@gmail.com>
2017-04-18 14:32:06 -0400

    Divided Classes

--- app/src/main/java/cs321/classcamapp/MainActivity.java
+++ app/src/main/java/cs321/classcamapp/MainActivity.java
@@ -1,7 +1,11 @@
 package cs321.classcamapp;
 
+import android.content.Intent;
+import android.support.transition.Scene;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openCamera(View view) {
+        Toast.makeText(this, "Open Camera", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, CameraScreen.class);
+        startActivity(intent);
+    }
+
+    public void openSchedule(View view) {
+        Toast.makeText(this, "Open Schedule", Toast.LENGTH_SHORT).show();Intent intent = new Intent(this, Scheduler.class);
+        startActivity(intent);
+    }
+
+    public void openFileManager(View view) {
+        Toast.makeText(this, "Open File Manager", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, FileManager.class);
+        startActivity(intent);
     }
 }
