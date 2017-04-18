0ce3b7b6a5f331ef3b8a3b9cff6b186140da0cfc
Flavio <FlavioAmurrioCS@gmail.com>
2017-04-18 18:37:57 -0400

    Added GridView

--- app/src/main/java/cs321/classcamapp/ImageAdapter.java
+++ app/src/main/java/cs321/classcamapp/ImageAdapter.java
@@ -42,7 +42,7 @@ public class ImageAdapter extends BaseAdapter {
         } else {
             imageView = (ImageView) convertView;
         }
-//        imageView.setImageResource(mThumbIds[position]);
+        imageView.setImageResource(mThumbIds[position]);
         return imageView;
     }
 //    public Object[] getImageList
@@ -50,6 +50,16 @@ public class ImageAdapter extends BaseAdapter {
     // references to our images
     private Integer[] mThumbIds = {
             R.drawable.fileicon, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+            R.drawable.sample_6, R.drawable.sample_7,
+            R.drawable.sample_0, R.drawable.sample_1,
+            R.drawable.sample_2, R.drawable.sample_3,
+            R.drawable.sample_4, R.drawable.sample_5,
+
             R.drawable.sample_4, R.drawable.sample_5,
             R.drawable.sample_6, R.drawable.sample_7,
             R.drawable.sample_0, R.drawable.sample_1,
