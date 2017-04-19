efcb6890641c35d1a00aacaebd3f2239e8f81804
Jonathan Perry <jperry16@masonlive.gmu.edu>
2017-04-19 15:38:33 -0400

    Finished checkClass method that returns name of class if currently in a class

--- app/src/main/java/cs321/classcamapp/MainActivity.java
+++ app/src/main/java/cs321/classcamapp/MainActivity.java
@@ -12,6 +12,9 @@ import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    //arrayList of schedules(className, startDate, endDate, startHour, startMinute, endHour, endMinute (base 24)
+    static ArrayList<Schedule> classSchedule = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -42,10 +45,30 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, FileManager.class);
         startActivity(intent);
     }
-        
+
+    /***
+     * Determines if the current time is currently within a scheduled class period
+     * @return Returns the class name if currently within a schedule class. Returns an empty string
+     * if not currently within a class period.
+     *
+     */
     public static String checkClass(){
         Date dt = new Date();
-        for(int i=0; i < )
-        //arrayList of schedules(className, startDate, endDate, startHour, startMinute, endHour, endMinute (base 24)
+        Schedule s;
+        for(int i=0; i < classSchedule.size() - 1; i++){
+            s = classSchedule.get(i);
+            int currentHour = dt.getHours();
+            int currentMin = dt.getMinutes();
+            if(dt.after(s.getStartDate()) && dt.before(s.getEndDate())){    //within callender date
+                if(( currentHour >= s.getStartHour()) && currentHour <= s.getEndHour()){    // within class period hour
+                    if(currentMin >= s.getStartMin() && currentMin <= s.getEndMin()){
+                        // If I get here then the we have verified that we are currently in s class
+                        return s.getClassName();
+                    }
+                }
+            }
+        }
+        return "";
+
     }
 }
