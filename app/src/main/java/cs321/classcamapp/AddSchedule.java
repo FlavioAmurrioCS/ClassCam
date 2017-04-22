73cd07492e22c84f74843e8307127062a7a936d2
My Le <mle23@gmu.edu>
2017-04-22 11:06:10 -0400

    dsf

---
+++ app/src/main/java/cs321/classcamapp/AddSchedule.java
@@ -0,0 +1,127 @@
+package cs321.classcamapp;
+
+import android.app.FragmentTransaction;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+public class AddSchedule extends AppCompatActivity {
+
+    EditText name, date_start, date_end, time_start, time_end;
+    CheckBox Mon, Tue, Wed, Thurs, Fri, Sat, Sun;
+    Button save, cancel;
+    FileIO fileio = new FileIO();
+    ArrayList<Class> classData = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_schedule);
+
+        classData = new ArrayList<>();
+        classData.clear();
+        classData.addAll(fileio.FileInput());
+
+        date_start =(EditText)findViewById(R.id.startDate);
+        date_end = (EditText) findViewById(R.id.endDate);
+        time_start = (EditText)findViewById(R.id.startTime);
+        time_end = (EditText)findViewById(R.id.endTime);
+        name = (EditText) findViewById(R.id.editName);
+
+        save = (Button)findViewById(R.id.save_bt);
+        cancel = (Button)findViewById(R.id.cancel_bt);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+
+        date_start.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            public void onFocusChange(View view, boolean hasfocus){
+                if(hasfocus){
+                    DateDialog dialog=new DateDialog(view);
+                    FragmentTransaction ft =getFragmentManager().beginTransaction();
+                    dialog.show(ft, "DatePicker");
+                }
+            }
+        });
+
+
+        date_end.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            public void onFocusChange(View view, boolean hasfocus){
+                if(hasfocus){
+                    DateDialog dialog=new DateDialog(view);
+                    FragmentTransaction ft =getFragmentManager().beginTransaction();
+                    dialog.show(ft, "DatePicker");
+                }
+            }
+        });
+
+        time_start.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            public void onFocusChange(View view, boolean hasfocus){
+                if(hasfocus){
+                    TimeDialog dialog = TimeDialog.newInstance(view);
+                    android.support.v4.app.FragmentTransaction ft =getSupportFragmentManager().beginTransaction();
+                    dialog.show(ft, "TimeDialog");
+
+                }
+            }
+        });
+
+
+        time_end.setOnFocusChangeListener(new View.OnFocusChangeListener(){
+            public void onFocusChange(View view, boolean hasfocus){
+                if(hasfocus){
+                    TimeDialog dialog = TimeDialog.newInstance(view);
+                    android.support.v4.app.FragmentTransaction ft =getSupportFragmentManager().beginTransaction();
+                    dialog.show(ft, "TimeDialog");
+
+                }
+            }
+        });
+    }
+
+    public void saveMethod(View view) {
+
+        Schedule cl = new Schedule();
+        cl.setClassName(name.toString());
+
+        cl.setStartDate(stringToDate(date_start.toString()));
+        cl.setEndDate(stringToDate(date_end.toString()));
+
+        int tem []= stringToTime(time_end.toString());
+        cl.setStartHour(tem[0]);
+        cl.setStartMin(tem[1]);
+
+        int tem2 []= stringToTime(time_start.toString());
+        cl.setEndHour(tem2[0]);
+        cl.setEndMin(tem2[1]);
+
+        classData.add(cl);
+
+    }
+
+    public Date stringToDate(String str){
+        String [] spl = str.split("/");
+        int day [] = new int[3];
+        for(int i = 0; i < spl.length; i ++){
+            day[i] = Integer.parseInt(spl[i]);
+        }
+        return new Date(day[2], day[0], day[1]);
+    }
+
+    public int[] stringToTime(String str){
+        String [] spl = str.split(":");
+        int time [] = new int[2];
+        for(int i = 0; i < 2; i++)
+            time[i] = Integer.parseInt(spl[i]);
+        return time;
+    }
+
+}
