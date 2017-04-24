36a5c6adc3b971043080b6b35645913552cfea80
Jonathan Perry <jperry16@masonlive.gmu.edu>
2017-04-24 14:43:15 -0400

    scrolling image viewing

--- app/src/main/java/cs321/classcamapp/ImageViewAdapter.java
+++ app/src/main/java/cs321/classcamapp/ImageViewAdapter.java
@@ -1,31 +1,57 @@
 package cs321.classcamapp;
 
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.ImageView;
+
+import java.io.File;
 
 /**
  * Created by Jonathan on 4/23/2017.
  */
 
 public class ImageViewAdapter extends BaseAdapter {
+
+    private Context mContext;
+
+    public ImageViewAdapter(Context context){
+        mContext = context;
+    }
     @Override
     public int getCount() {
-        return 0;
+        return ImageBrowser.getNoteArray().length;
     }
 
     @Override
     public Object getItem(int i) {
-        return null;
+        return i;
     }
 
     @Override
     public long getItemId(int i) {
-        return 0;
+        return i;
     }
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ImageView img = new ImageView(mContext);
+
+        File imageFile = new File(ImageBrowser.getNoteArray()[i].getFileLink());
+        Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+        img.setImageBitmap(bitmap);
+
+
+        img.setLayoutParams(new Gallery.LayoutParams(200, 200));
+
+        img.setScaleType(ImageView.ScaleType.FIT_XY);
+
+
+        return img;
+
     }
 }
