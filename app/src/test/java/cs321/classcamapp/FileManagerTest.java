1ec9bc914f210c8c6062f465de9014371953e137
Tor Fredericks <tfreder4@masonlive.gmu.edu>
2017-04-30 12:23:24 -0400

    Update Activity

---
+++ app/src/test/java/cs321/classcamapp/FileManagerTest.java
@@ -0,0 +1,27 @@
+package cs321.classcamapp;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tor on 4/30/17.
+ */
+public class FileManagerTest {
+    @Before
+    public void setup() throws Exception {
+
+    }
+
+    @Test
+    public void getFolderName() throws Exception {
+
+    }
+
+    @Test
+    public void onCreate() throws Exception {
+
+    }
+
+}
