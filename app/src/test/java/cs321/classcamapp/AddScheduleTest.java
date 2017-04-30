cdd87ba86e28b1310819e42a9713a558d116f972
Flavio <FlavioAmurrioCS@gmail.com>
2017-04-30 15:42:49 -0400

    Addead new extends

--- app/src/test/java/cs321/classcamapp/AddScheduleTest.java
+++ app/src/test/java/cs321/classcamapp/AddScheduleTest.java
@@ -1,10 +1,30 @@
 package cs321.classcamapp;
 
+import android.test.ActivityInstrumentationTestCase2;
+
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 /**
  * Created by MyLe on 4/30/17.
  */
-public class AddScheduleTest {
+public class AddScheduleTest extends ActivityInstrumentationTestCase2<FileManager> {
+    AddSchedule addSchedule;
 
+    public AddScheduleTest(Class<FileManager> activityClass) {
+        super(activityClass);
+    }
+
+
+    @Before
+    public void setup() throws Exception {
+
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 }
