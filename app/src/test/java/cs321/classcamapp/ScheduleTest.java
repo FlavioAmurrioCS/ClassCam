20fbd4a2a10ac9f54ec1c5028049029f7bbdf4e4
Tor Fredericks <tfreder4@masonlive.gmu.edu>
2017-04-30 12:34:20 -0400

    Update Activity

---
+++ app/src/test/java/cs321/classcamapp/ScheduleTest.java
@@ -0,0 +1,131 @@
+package cs321.classcamapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by tor on 4/30/17.
+ */
+public class ScheduleTest {
+    @Test
+    public void clearWeek() throws Exception {
+
+    }
+
+    @Test
+    public void toListView() throws Exception {
+
+    }
+
+    @Test
+    public void dateToString() throws Exception {
+
+    }
+
+    @Test
+    public void toFileString() throws Exception {
+
+    }
+
+    @Test
+    public void startTimeToString() throws Exception {
+
+    }
+
+    @Test
+    public void endTimeToString() throws Exception {
+
+    }
+
+    @Test
+    public void getClassName() throws Exception {
+
+    }
+
+    @Test
+    public void setClassName() throws Exception {
+
+    }
+
+    @Test
+    public void getStartDate() throws Exception {
+
+    }
+
+    @Test
+    public void setStartDate() throws Exception {
+
+    }
+
+    @Test
+    public void getEndDate() throws Exception {
+
+    }
+
+    @Test
+    public void setEndDate() throws Exception {
+
+    }
+
+    @Test
+    public void getStartHour() throws Exception {
+
+    }
+
+    @Test
+    public void setStartHour() throws Exception {
+
+    }
+
+    @Test
+    public void getEndHour() throws Exception {
+
+    }
+
+    @Test
+    public void setEndHour() throws Exception {
+
+    }
+
+    @Test
+    public void getStartMin() throws Exception {
+
+    }
+
+    @Test
+    public void setStartMin() throws Exception {
+
+    }
+
+    @Test
+    public void getEndMin() throws Exception {
+
+    }
+
+    @Test
+    public void setEndMin() throws Exception {
+
+    }
+
+    @Test
+    public void addWeek() throws Exception {
+
+    }
+
+    @Test
+    public void getWeekDay() throws Exception {
+
+    }
+
+    @Test
+    public void classDBInput() throws Exception {
+
+    }
+
+    @Test
+    public void classDBOutout() throws Exception {
+
+    }
+
+}
